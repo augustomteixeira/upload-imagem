7d1d16ee6cdc3a951059b1051cf53b83f9927811
Marcelo Augusto <augustomnzteixeira@gmail.com>
2020-12-05 19:31:38 -0300

    first commit

---
+++ src/main/java/com/augustomteixeira/uploadimagem/UploadimagemApplication.java
@@ -0,0 +1,13 @@
+package com.augustomteixeira.uploadimagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadimagemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadimagemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/augustomteixeira/uploadimagem/UploadimagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustomteixeira.uploadimagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadimagemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
