9f3097f7ad63e95fc851d6d50fcaceb3cfc3d1a8
Marcelo Augusto <augustomnzteixeira@gmail.com>
2020-12-08 19:09:09 -0300

    criação do controlador e do template principal

---
+++ src/main/java/com/augustomteixeira/uploadimagem/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.augustomteixeira.uploadimagem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("inicio/index");
+        return mv;
+    }
+
+}
