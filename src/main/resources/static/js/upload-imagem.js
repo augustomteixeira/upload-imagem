dbfdd21845497f23bd6c8e0144d9d0a9dccb1a5d
Marcelo <augustomnzteixeira@gmail.com>
2021-01-18 10:36:32 -0300

    preview da imagem no modal

--- src/main/resources/static/js/upload-imagem.js
+++ src/main/resources/static/js/upload-imagem.js
@@ -1,3 +1,22 @@
 $(document).on('click', '#button-modal-upload', function (e) {
 	$('#modal-upload-imagem').modal('show');
 });
+
+$(document).on('change', '#input-carregar-imagem', function (e) {
+	lerInput(this);
+});
+
+
+
+function lerInput(input) {
+	if (input.files && input.files[0]) { 
+		var reader = new FileReader();
+		
+		reader.onload = function(e) {
+			$('#vizualizar-imagem-modal').attr('src', e.target.result);
+			$('#vizualizar-imagem-modal').show();
+		}
+		
+		reader.readAsDataURL(input.files[0]); //converte a imagem para base64
+	}
+}
