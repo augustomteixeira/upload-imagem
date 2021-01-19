b2a12cb7f343d9c67182d03a077c88e5299dbbec
Marcelo <augustomnzteixeira@gmail.com>
2021-01-19 09:22:56 -0300

    repository para a tabela imagem

---
+++ src/main/java/com/augustomteixeira/uploadimagem/repository/ImagemRepository.java
@@ -0,0 +1,11 @@
+package com.augustomteixeira.uploadimagem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sun.prism.Image;
+
+@Repository
+public interface ImagemRepository extends JpaRepository<Image, Integer> {
+
+}
